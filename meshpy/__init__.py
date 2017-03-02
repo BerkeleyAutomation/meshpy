ad01c1e0b04c33c4d4ec3d53ea7e03e8c123ef33
Jeffrey Brian Mahler <jmahler@imail.EECS.Berkeley.EDU>
2017-03-01 19:57:52 -0800

    Made meshrender optional

--- meshpy/__init__.py
+++ meshpy/__init__.py
@@ -1,6 +1,9 @@
-import meshrender
+try:
+    import meshrender
+except:
+    print 'Unable to import meshrender shared library! Rendering will not work. Likely due to missing Boost.Numpy'
+    print 'Boost.Numpy can be installed following the instructions in https://github.com/ndarray/Boost.NumPy'
 from mesh import Mesh3D
-from mesh_renderer import ViewsphereDiscretizer, PlanarWorksurfaceDiscretizer, VirtualCamera, SceneObject
 from obj_file import ObjFile
 from off_file import OffFile
 from sdf import Sdf, Sdf3D
@@ -8,7 +11,12 @@ from sdf_file import SdfFile
 from stable_pose import StablePose
 from stp_file import StablePoseFile
 
-from random_variables import CameraSample, RenderSample, UniformViewsphereRandomVariable, UniformPlanarWorksurfaceRandomVariable, UniformPlanarWorksurfaceImageRandomVariable
+try:
+    from mesh_renderer import ViewsphereDiscretizer, PlanarWorksurfaceDiscretizer, VirtualCamera, SceneObject
+    from random_variables import CameraSample, RenderSample, UniformViewsphereRandomVariable, UniformPlanarWorksurfaceRandomVariable, UniformPlanarWorksurfaceImageRandomVariable
+except:
+    print 'Unable to import mesh rendering! Likely due to missing Boost.Numpy'
+    print 'Boost.Numpy can be installed following the instructions in https://github.com/ndarray/Boost.NumPy'
 
 __all__ = ['Mesh3D',
            'ViewsphereDiscretizer', 'PlanarWorksurfaceDiscretizer', 'VirtualCamera', 'SceneObject',
