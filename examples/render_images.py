Added edge weights to vertex normal computation and fixed bug in vertex alignment that was only using a single vertex normal on the convex hull to determine alignment. It still assumes that the triangles are oriented coherently... but fuck it

examples/render_images.py
@@ -12,7 +12,7 @@ import sys
 import time
 
 import core.utils as utils
-from core import RigidTransform
+from core import NormalCloud, PointCloud, RigidTransform
 from perception import CameraIntrinsics, ObjectRender, RenderMode
 from meshpy import MaterialProperties, LightingProperties, ObjFile, VirtualCamera, ViewsphereDiscretizer, SceneObject
 
@@ -26,6 +26,8 @@ if __name__ == '__main__':
     parser.add_argument('mesh_filename', type=str, help='filename for .OBJ mesh file to render')
     args = parser.parse_args()
 
+    vis_normals = True
+
     # read data
     mesh_filename = args.mesh_filename
     _, mesh_ext = os.path.splitext(mesh_filename)
@@ -33,13 +35,21 @@ if __name__ == '__main__':
         raise ValueError('Must provide mesh in Wavefront .OBJ format!') 
     orig_mesh = ObjFile(mesh_filename).read()
     mesh = orig_mesh.subdivide(min_tri_length=0.01)
+    mesh.compute_vertex_normals()
     stable_poses = mesh.stable_poses()
+
+    if vis_normals:
+        vis3d.figure()
+        vis3d.mesh(mesh)
+        vis3d.normals(NormalCloud(mesh.normals.T), PointCloud(mesh.vertices.T), subsample=10)
+        vis3d.show()
 
     d = utils.sqrt_ceil(len(stable_poses))
     vis.figure(size=(16,16))
 
     table_mesh = ObjFile('data/meshes/table.obj').read()
     table_mesh = table_mesh.subdivide()
+    table_mesh.compute_vertex_normals()
     table_mat_props = MaterialProperties(color=(0,255,0),
                                          ambient=0.5,
                                          diffuse=1.0,
@@ -81,8 +91,6 @@ if __name__ == '__main__':
         for name, scene_obj in scene_objs.iteritems():
             virtual_camera.add_to_scene(name, scene_obj)
         
-        #mesh = mesh.merge(table_mesh.transform(T_obj_world.inverse()))
-
         # camera pose
         cam_dist = 0.3
         T_camera_world = RigidTransform(rotation=np.array([[0, 1, 0],

meshpy/mesh.py
@@ -563,30 +563,78 @@ class Mesh3D(object):
             tris = self.triangles[inds[0],:]
             normal = np.zeros(3)
             for tri in tris:
+                # compute triangle normal
                 t = self.vertices[tri, :]
                 v0 = t[1,:] - t[0,:]
                 v1 = t[2,:] - t[0,:]
+                if np.linalg.norm(v0) == 0:
+                    continue
                 v0 = v0 / np.linalg.norm(v0)
+                if np.linalg.norm(v1) == 0:
+                    continue
                 v1 = v1 / np.linalg.norm(v1)
                 n = np.cross(v0, v1)
+                if np.linalg.norm(n) == 0:
+                    continue
                 n = n / np.linalg.norm(n)
-                w = self._area_of_tri(tri)
-                normal += w * n
+
+                # compute weight by area of triangle
+                w_area = self._area_of_tri(tri)
+
+                # compute weight by edge angle
+                vertex_ind = np.where(tri == i)[0][0]
+                if vertex_ind == 0:
+                    e0 = t[1,:] - t[0,:]
+                    e1 = t[2,:] - t[0,:]
+                elif vertex_ind == 1:
+                    e0 = t[0,:] - t[1,:]
+                    e1 = t[2,:] - t[1,:]
+                elif vertex_ind == 2:
+                    e0 = t[0,:] - t[2,:]
+                    e1 = t[1,:] - t[2,:]
+                if np.linalg.norm(e0) == 0:
+                    continue
+                if np.linalg.norm(e1) == 0:
+                    continue
+                e0 = e0 / np.linalg.norm(e0)
+                e1 = e1 / np.linalg.norm(e1)
+                w_angle = np.arccos(e0.dot(e1))
+
+                # weighted update
+                # www.bytehazard.com/articles/vertnorm.html
+                normal += w_area * w_angle * n
+
+            if np.linalg.norm(normal) == 0:
+                normal = np.array([1,0,0])
             normal = normal / np.linalg.norm(normal)
             normals.append(normal)
 
-        # Reverse normals based on alignment with convex hull
-        hull = ss.ConvexHull(self.vertices_)
-        hull_tris = hull.simplices.tolist()
-        hull_vertex_ind = hull_tris[0][0]
-        hull_vertex = self.vertices[hull_vertex_ind]
-        hull_vertex_normal = normals[hull_vertex_ind]
-        v = np.array(hull_vertex).reshape([1,3])
-        n = np.array(hull_vertex_normal)
-        ip = (self.vertices - np.tile(hull_vertex, [self.vertices.shape[0], 1])).dot(n)
-        if ip[0] > 0:
-            normals = [[-n[0], -n[1], -n[2]] for n in normals]
+        # set numpy array
         self.normals = np.array(normals)
+
+        # reverse normals based on alignment with convex hull
+        hull = ss.ConvexHull(self.vertices)
+        hull_tris = hull.simplices.tolist()
+        hull_vertex_inds = np.unique(hull_tris)
+
+        num_aligned = 0
+        num_misaligned = 0
+        for hull_vertex_ind in hull_vertex_inds:
+            hull_vertex = self.vertices[hull_vertex_ind, :]
+            hull_vertex_normal = normals[hull_vertex_ind]
+            ip = (hull_vertex - self.vertices).dot(hull_vertex_normal)
+            num_aligned += np.sum(ip > 0)
+            num_misaligned += np.sum(ip <= 0)
+
+        if num_misaligned > num_aligned:
+            self.normals = -self.normals
+
+    def flip_normals(self):
+        """ Flips the mesh normals. """
+        if self.normals is not None:
+            self.normals = -self.normals
+            return True
+        return False
         
     def scale_principal_eigenvalues(self, new_evals):
         self.normalize_vertices()